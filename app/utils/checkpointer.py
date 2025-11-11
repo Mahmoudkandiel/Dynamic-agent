02b3843876efa60bfe559b3b1c54add35f458e98
Mahmoud Kandiel <mahmoudkandiel8@gmail.com>
2025-11-11 13:58:46 +0200

    Update checkpointer.py

--- app/utils/checkpointer.py
+++ app/utils/checkpointer.py
@@ -1,7 +1,7 @@
 from langgraph.checkpoint.mongodb import MongoDBSaver
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://tariqeyego:K0tn94fPWbB3XWKR@eyego.6gk2cxc.mongodb.net/?retryWrites=true&w=majority&appName=eyego")
 db = client["eyego"]
 langgraph_collection = db["langgraph_checkpoints"]
 
